559bcee5728456e32bada8fa4045e783d5aa5ab8
Khalmatov <large000@yandex.ru>
2022-04-16 22:19:12 +0300

    Задание 28.1.19

--- LifeSpot/JS/Index.js
+++ LifeSpot/JS/Index.js
@@ -1,2 +1,9 @@
 ﻿alert("Приветствуем на LifeSpot! " +
     new Date().toLocaleString());
+
+let age = prompt('Введите возраст:');
+if (age >= 18) alert('Добро пожаловать!');
+else {
+    alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+    window.location.href = "http://www.google.com";
+}
