b024a18549eccf67466ca5d65c0e55030aad3a85
Khalmatov <large000@yandex.ru>
2022-04-18 23:38:13 +0300

    Задание 28.5.3

--- LifeSpot/Static/JS/testing.js
+++ LifeSpot/Static/JS/testing.js
@@ -1,2 +1,5 @@
-﻿let name = prompt('Как вас зовут ?');
-alert('Приветствуем тебя ' + name + ' в твоем имени ' + name.length + ' символов');
+﻿//let name = prompt('Как вас зовут ?');
+let elements = document.getElementsByTagName('*');
+//console.log(elements);
+//alert('Приветствуем тебя ' + name + ' в твоем имени ' + name.length + ' символов' + ' а на странице ' + elements.length + ' элементов');
+alert(`Количество элементов на странице:  ${elements.length}`);
