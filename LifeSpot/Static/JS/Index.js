e7b63a45d85954c1945a4d9c5416652037c1dcbc
Khalmatov <large000@yandex.ru>
2022-04-22 00:17:31 +0300

    Задание 29.2.5

--- LifeSpot/Static/JS/Index.js
+++ LifeSpot/Static/JS/Index.js
@@ -1,4 +1,4 @@
-﻿function filterByVideoName() {
+﻿const filterByVideoName = function() {
     let userText = document.getElementsByTagName('input');
     let filter = userText[0].value.toLowerCase();
     let elements = document.getElementsByClassName('video-container');
@@ -17,21 +17,21 @@
     }
 }
 
-//function checkClientAge() {
-//    alert("Приветствуем на LifeSpot! " +
-//        new Date().toLocaleString());
-//    let userData = new Map();
-//    userData.set('browserData', window.navigator.userAgent);
-//    userData.set('dateTime', new Date().toLocaleString());
-//    let age = prompt('Введите возраст:');
-//    userData.set('userAge', age);
-//    if (age >= 18) alert('Добро пожаловать!');
-//    else {
-//        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-//        window.location.href = "http://www.google.com";
-//    }
+const checkClientAge = function() {
+    alert("Приветствуем на LifeSpot! " +
+        new Date().toLocaleString());
+    let userData = new Map();
+    userData.set('browserData', window.navigator.userAgent);
+    userData.set('dateTime', new Date().toLocaleString());
+    let age = prompt('Введите возраст:');
+    userData.set('userAge', age);
+    if (age >= 18) alert('Добро пожаловать!');
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com";
+    }
 
-//    console.log('browserData:' + userData.get('browserData'));
-//    console.log('dateTime:' + userData.get('dateTime'));
-//    console.log('userAge:' + userData.get('userAge'));
-//}
+    console.log('browserData:' + userData.get('browserData'));
+    console.log('dateTime:' + userData.get('dateTime'));
+    console.log('userAge:' + userData.get('userAge'));
+}
