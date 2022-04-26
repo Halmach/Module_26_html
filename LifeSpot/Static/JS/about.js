97168a8141f4d42d6ac61a8a03b462655bee2a85
Khalmatov <large000@yandex.ru>
2022-04-27 00:09:25 +0300

    Задание 29.7.5

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -1,1 +1,26 @@
 ﻿
+
+let GetReviewFromUser = function () {
+    let reviews = new Map();
+
+    let name = prompt('Введите ваше имя:');
+    reviews.set('name', name);
+
+    let review = prompt('Введите текст комментария:');
+    reviews.set('review', review);
+
+    reviews.set('date', new Date().toLocaleString());
+
+    return reviews;
+}
+
+let InsertReviewIntoReviewsContaiver = reviews => {
+    let elem = document.getElementsByClassName('reviews-container')[0];
+    elem.innerHTML += '<div class="review-text">';
+    elem.innerHTML += '<p>';
+    elem.innerHTML += 'Дата: ' + reviews.get('date') + '<br>';
+    elem.innerHTML += 'Пользователь: ' + reviews.get('name') + '<br>';
+    elem.innerHTML += 'Комментарий: ' + reviews.get('review') + '<br>';
+    elem.innerHTML += '</p>';
+    elem.innerHTML += '</div>';
+}
