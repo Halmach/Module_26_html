7f03adacd9db9738b5753c178de9499de7d71fcf
Khalmatov <large000@yandex.ru>
2022-05-01 14:43:30 +0300

    Задание 30.3.2

--- LifeSpot/Static/JS/testing.js
+++ LifeSpot/Static/JS/testing.js
@@ -3,3 +3,11 @@ let elements = document.getElementsByTagName('*');
 //console.log(elements);
 //alert('Приветствуем тебя ' + name + ' в твоем имени ' + name.length + ' символов' + ' а на странице ' + elements.length + ' элементов');
 alert(`Количество элементов на странице:  ${elements.length}`);
+
+
+const SaveInput = function () {
+    let currentInput = document.getElementsByTagName('input')[0].value.toLowerCase();
+    alert('Последний ввод: ' + this.lastInput /* равноценно window.lastInput, так как мы работаем в контексте браузера */
+        + '\n' + 'Текущий ввод: ' + currentInput);
+    this.lastInput = currentInput;
+}
